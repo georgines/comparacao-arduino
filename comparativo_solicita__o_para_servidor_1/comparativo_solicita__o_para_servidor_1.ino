506d7a06800fdafcb4ad5ddbc5e825c21a5bcc9a
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2016-10-03 18:29:59 -0300

    Adicionado os arquivos via upload

---
+++ comparativo_solicita__o_para_servidor_1/comparativo_solicita__o_para_servidor_1.ino
@@ -0,0 +1,61 @@
+#include <Ethernet.h>
+#include <SPI.h>
+#include "RestClient.h"
+
+RestClient client = RestClient("shrouded-inlet-53662.herokuapp.com"); //url para solicitação http
+
+unsigned long  tempoAtual, tempoAnterior, tempoResposta = 0;
+unsigned long integrador, falha, sucesso = 0;
+unsigned int contador;
+String resposta;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Conectando a internet...");
+  client.dhcp(); //obtendo ip dinamico da rede local
+  Serial.println("Pronto para comecar!");
+}
+
+void loop() {
+
+  for (contador = 1; contador < 11; contador++) { // loop de solicitações
+
+    resposta = "";
+    tempoAnterior = tempoAtual;
+
+    // método responsavel pela solicitação POST via http
+    int codigoDeEstatus = client.post("/aluno", "matricula=201510331826591", &resposta);
+
+    tempoAtual = millis(); //obtenção do tempo de execução
+    tempoResposta = tempoAtual - tempoAnterior; //cálculo tempo de resposta de uma solicitação
+
+    Serial.print("Solicitacao: #");
+    Serial.println(contador);
+
+    if (codigoDeEstatus == 200) {
+      sucesso++;
+      integrador += tempoResposta; //somatório de tempo de resposta
+      Serial.print("Resposta em: ");
+      Serial.print(tempoResposta);
+      Serial.println("ms");
+    } else {
+      falha++;
+      Serial.print("Sem resposta!");
+    }
+
+  }
+  Serial.println("Placa em execucao: Arduino UNO");
+  //  Serial.println("Placa em execucao: Arduino MEGA");
+  //  Serial.println("Placa em execucao: Arduino DUE");
+  Serial.print("Sucesso: ");
+  Serial.print(sucesso);
+  Serial.print(" ");
+  Serial.print("Falhas: ");
+  Serial.println(falha);
+  Serial.print("O tempo medio de resposta: ");
+  Serial.print(integrador / (sucesso)); //média de tempo de resposta
+  Serial.println("ms");
+  while (true);
+
+
+}
---
+++ comparativo_velocidade_calculo_pi/comparativo_velocidade_calculo_pi.ino
@@ -0,0 +1,45 @@
+#include "math.h"
+
+unsigned long tempoDecorrido;
+unsigned long inicio;
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  inicio = millis();
+
+  double n, i;
+  double s = 1;
+  double pi = 3;
+
+
+  n = 100000; //quantidade de interações
+
+  for (i = 2; i <= n * 2; i += 2) { //loop para o cálculo de PI
+    pi = pi + s * (4 / (i * (i + 1) * (i + 2)));
+    s = -s;
+  }
+
+  tempoDecorrido = millis(); //retorno de tempo de execução
+
+  //  Serial.println("Placa em execucao: Arduino UNO");
+  //  Serial.println("Placa em execucao: Arduino MEGA");
+  //  Serial.println("Placa em execucao: Arduino DUE");
+  Serial.println("Numero aproximado de PI usando a serie de Nilakantha seculo 15");
+  Serial.println("Interacoes: 100000");
+  Serial.print("Iniciado em: ");
+  Serial.print(inicio);
+  Serial.println("ms");
+  Serial.print("PI = ");
+  Serial.println(pi, 16);
+  Serial.print("Tempo decorrido: ");
+  Serial.print(tempoDecorrido);
+  Serial.println("ms");
+
+  while (true);
+}
+
+
+
+
